e373f6eeb2bb2bad8923c68379e932ee467c2f44
asm-jaime <asm.jaime@gmail.com>
2023-06-04 22:58:37 +1000

    add index.html and component.js that you can load with no package manager or any transpiling

---
+++ component.js
@@ -0,0 +1,7 @@
+import React, { useEffect} from 'react';
+
+export const FunctionalComponent = () => {
+    return(<div>EventCategory</div>);
+};
+
+window.FunctionalComponent = FunctionalComponent;
